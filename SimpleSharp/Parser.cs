f6c70f30f466550a99002d8f3b985a1b71361cdf
Mikah <mikah@mltd.com>
2022-12-23 21:03:35 -0800

    Added more to the parser class

--- SimpleSharp/Parser.cs
+++ SimpleSharp/Parser.cs
@@ -9,10 +9,10 @@ namespace SimpleSharp
 {
     public enum NonTerminalStates
     {
-        MathAddSub,  //MathAddSub  -> MathMultDiv    + MathAddSub     | MathMultDiv           -  MathAddSub      | MathMultDiv
-        MathMultDiv, //MathMultDiv -> MathExpLog     * MathMultDiv    | MathExpLog            /  MathMultDiv     | MathExpLog
-        MathExpLog,  //MathExpLog  -> MathExpression ^ MathExpression | MathExpression[base] log MathExpression  | MathExpression
-        MathExpression, //MathExpression -> (Math1) | Identifier[Token] | Number[Token]
+        MathAddSub,  //MathAddSub  -> MathMultDiv    + MathAddSub  | MathMultDiv           -  MathAddSub  | MathMultDiv
+        MathMultDiv, //MathMultDiv -> MathExpLog     * MathMultDiv | MathExpLog            /  MathMultDiv | MathExpLog
+        MathExpLog,  //MathExpLog  -> MathExpression ^ MathExpLog  | MathExpression[base] log MathExpLog  | MathExpression
+        MathExpression, //MathExpression -> (MathAddSub) | Identifier[Token] | Number[Token] | AddSub[Token] Number[Token]
     }
 
     public class ParserNode
@@ -41,45 +41,62 @@ namespace SimpleSharp
         public ParserNode Head;
         public Dictionary<NonTerminalStates, List<Func<ParserNode[]>>> MakeChildrenFunctions;
 
-        //MathAddSub  -> MathMultDiv    + MathAddSub     | MathMultDiv           -  MathAddSub      | MathMultDiv
-        //MathMultDiv -> MathExpLog     * MathMultDiv    | MathExpLog            /  MathMultDiv     | MathExpLog
-        //MathExpLog  -> MathExpression ^ MathExpression | MathExpression[base] log MathExpression  | MathExpression
-        //MathExpression -> (Math1) | Identifier[Token] | Number[Token]
+        //MathAddSub  -> MathMultDiv    + MathAddSub  | MathMultDiv           -  MathAddSub  | MathMultDiv
+        //MathMultDiv -> MathExpLog     * MathMultDiv | MathExpLog            /  MathMultDiv | MathExpLog
+        //MathExpLog  -> MathExpression ^ MathExpLog  | MathExpression[base] log MathExpLog  | MathExpression
+        //MathExpression -> (MathAddSub) | Identifier[Token] | Number[Token] | AddSub[Token] Number[Token]
 
-        //public Parser(Token[] tokens)
-        //{
-        //    Tokens = tokens;
-        //    Head = Parse();
-        //    MakeChildrenFunctions = new Dictionary<NonTerminalStates, List<Func<ParserNode[]>>>();
-        //    List<Func<ParserNode[]>> mathAddSub = new List<Func<ParserNode[]>>();
-        //    mathAddSub.Add(() => new ParserNode[] { new ParserNode(NonTerminalStates.MathMultDiv), new ParserNode(Classifications.Operator), new ParserNode(NonTerminalStates.MathAddSub) });
-        //    mathAddSub.Add(() => new ParserNode[] { new ParserNode(NonTerminalStates.MathMultDiv), new ParserNode(Classifications.Operator), new ParserNode(NonTerminalStates.MathAddSub) });
-        //    mathAddSub.Add(() => new ParserNode[] { new ParserNode(NonTerminalStates.MathMultDiv) });
-        //    MakeChildrenFunctions.Add(NonTerminalStates.MathAddSub, );
-        //}
+        public Parser(Token[] tokens)
+        {
+            Tokens = tokens;
+            Head = Parse();
+            MakeChildrenFunctions = new Dictionary<NonTerminalStates, List<Func<ParserNode[]>>>();
 
-        //private ParserNode Parse()
-        //{
-        //    List<Token> currentLine = new List<Token>();
-        //    List<ParserNode> parsedLines = new List<ParserNode>();
-        //    for(int i = 0; i < Tokens.Length; i ++)
-        //    {
-        //        if (Tokens[i].Classification == Classifications.CodeSeparator)
-        //        {
-        //            parsedLines.Add(ParseLine(currentLine));
-        //            currentLine = new List<Token>();
-        //        }
-        //        else
-        //        {
-        //            currentLine.Add(Tokens[i]);
-        //        }
-        //    }
-        //}
+            List<Func<ParserNode[]>> mathAddSub = new List<Func<ParserNode[]>>();
+            mathAddSub.Add(() => new ParserNode[] { new ParserNode(NonTerminalStates.MathMultDiv), new ParserNode(Classifications.AddSub), new ParserNode(NonTerminalStates.MathAddSub) });
+            mathAddSub.Add(() => new ParserNode[] { new ParserNode(NonTerminalStates.MathMultDiv) });
+            MakeChildrenFunctions.Add(NonTerminalStates.MathAddSub, mathAddSub);
 
-        //private ParserNode ParseLine(List<Token> targetLine)
-        //{
-        //    int currentIndex = 0; 
+            List<Func<ParserNode[]>> mathMultDiv = new List<Func<ParserNode[]>>();
+            mathMultDiv.Add(() => new ParserNode[] { new ParserNode(NonTerminalStates.MathExpLog), new ParserNode(Classifications.MultDiv), new ParserNode(NonTerminalStates.MathMultDiv) });
+            mathMultDiv.Add(() => new ParserNode[] { new ParserNode(NonTerminalStates.MathExpLog) });
+            MakeChildrenFunctions.Add(NonTerminalStates.MathMultDiv, mathMultDiv);
 
-        //}
+            List<Func<ParserNode[]>> mathExpLog = new List<Func<ParserNode[]>>();
+            mathExpLog.Add(() => new ParserNode[] { new ParserNode(NonTerminalStates.MathExpression), new ParserNode(Classifications.ExpLog), new ParserNode(NonTerminalStates.MathExpLog) });
+            mathExpLog.Add(() => new ParserNode[] { new ParserNode(NonTerminalStates.MathExpression) });
+            MakeChildrenFunctions.Add(NonTerminalStates.MathExpLog, mathExpLog);
+
+            List<Func<ParserNode[]>> mathExpression = new List<Func<ParserNode[]>>();
+            mathExpression.Add(() => new ParserNode[] { new ParserNode(Classifications.LeftParenthesis), new ParserNode(NonTerminalStates.MathAddSub), new ParserNode(Classifications.RightParenthesis) });
+            mathExpression.Add(() => new ParserNode[] { new ParserNode(Classifications.Identifier) });
+            mathExpression.Add(() => new ParserNode[] { new ParserNode(Classifications.Number) });
+            mathExpression.Add(() => new ParserNode[] { new ParserNode(Classifications.AddSub), new ParserNode(Classifications.Number) });
+            MakeChildrenFunctions.Add(NonTerminalStates.MathExpression, mathExpression);
+        }
+
+        private ParserNode Parse()
+        {
+            List<Token> currentLine = new List<Token>();
+            List<ParserNode> parsedLines = new List<ParserNode>();
+            for (int i = 0; i < Tokens.Length; i++)
+            {
+                if (Tokens[i].Classification == Classifications.CodeSeparator)
+                {
+                    parsedLines.Add(ParseLine(currentLine));
+                    currentLine = new List<Token>();
+                }
+                else
+                {
+                    currentLine.Add(Tokens[i]);
+                }
+            }
+        }
+
+        private ParserNode ParseLine(List<Token> targetLine)
+        {
+            int currentIndex = 0;
+
+        }
     }
 }
